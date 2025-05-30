b8176333ca472360d8f5f7108a4e98603d971e8c
Narendra337 <aradalanarendra@gmail.com>
2025-05-30 23:26:40 +0530

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "nareshawsdevvvv"
-    key    = "ec2/terraform.tfstate"
-    region = "us-east-1"
-  }
+  
 
   required_version = ">= 1.6.3"
 }
